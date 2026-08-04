8431910a496efa6335b17d37ffaabcd11a423726
MuhammadHassanAnsari <muhammadhassanansari2008@gmail.com>
2026-08-05 00:43:48 +0500

    Tic Tac Complete

--- TIC TAC/script.js
+++ TIC TAC/script.js
@@ -1,7 +1,10 @@
 let boxes = document.querySelectorAll(".box");
+let button = document.getElementById("restart");
+
 
 let currentPlayer = "X";
 let gameOver = false;
+
 let winningCombination = [
     [0, 1, 2],
     [3, 4, 5],
@@ -10,7 +13,7 @@ let winningCombination = [
     [2, 4, 6],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 3, 8]
+    [2, 5, 8]
 ];
 
 boxes.forEach((boxitem) => {
@@ -20,15 +23,16 @@ boxes.forEach((boxitem) => {
         }
         if (boxitem.innerText === "") {
             boxitem.innerText = currentPlayer;
+            if (currentPlayer == "X") {
+                currentPlayer = "O";
+            }
+
+            else {
+                currentPlayer = "X";
+            }
         }
 
-        if (currentPlayer == "X") {
-            currentPlayer = "O";
-        }
 
-        else {
-            currentPlayer = "X";
-        }
 
         let firstBox;
         let secondBox;
@@ -46,11 +50,43 @@ boxes.forEach((boxitem) => {
                 gameOver = true;
             }
         });
+        let filledboxs = 0;
 
-    })
+        boxes.forEach((fillBox) => {
+            if (fillBox.innerText !== "") {
+                filledboxs = filledboxs + 1;
+
+            }
+        });
+
+        if (filledboxs == 9 && !gameOver) {
+            alert("Draw");
+            gameOver = true;
+        }
+    });
 
 
 });
+
+
+button.addEventListener("click", () => {
+    boxes.forEach((boxs) => {
+        boxs.innerText = "";
+
+    }
+    );
+
+    currentPlayer = "X";
+
+    gameOver = false;
+});
+
+
+
+
+
+
+
 
 
 
