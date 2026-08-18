99e1a21aa3388351972a27079de4d88d7657c1cc
MuhammadHassanAnsari <muhammadhassanansari2008@gmail.com>
2026-08-18 23:28:41 +0500

    Date Time and Numbers

---
+++ Lecture/Date and Time/script.js
@@ -0,0 +1,67 @@
+// LIERAL METHOD 
+
+// let num = 12343223n;
+// let num1 = 1234.43
+// console.log(num)
+// console.log(num1)
+
+
+// CONSTRUCTOR METHOD 
+
+// let num = new Number(2345654);
+// console.log(num)
+
+
+// NUMBER PROPERTIES 
+
+// let a = 0/0;
+// console.log(a);  
+// console.log(isNaN(a))
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.POSITIVE_INFINITY)
+// console.log(Number.NEGATIVE_INFINITY)
+// // console.log(Number.NaN) Not a Number
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Number.EPSILON)
+
+
+
+// NUMBER METHOD 
+// 1- isNaN
+// let a = 0/0;
+// console.log(a);  
+// console.log(isNaN(a))
+
+
+// let num = 12.54;
+// let num1 = '124';
+
+// FLOAT ----> Integer
+
+// console.log(Number.parseInt(num));
+
+// console.log(Number.parseFloat(num1));
+
+// console.log(Number.isFinite(1234));
+
+
+
+
+// Number Instace Method 
+
+
+let num = 14;
+
+// (3) hume bata ta hai decimal ke bad kitne digit ayenge aur round off bhi krta hai
+console.log(num.toFixed(3))
+
+
+
+
+num.toExponential(num);
+num.toExponential(num);
+num.toPrecision(num);
+num.toString(num);
