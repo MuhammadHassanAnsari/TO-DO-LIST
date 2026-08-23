b7c812eab899f532702d81924f17f698045280cb
MuhammadHassanAnsari <muhammadhassanansari2008@gmail.com>
2026-08-24 01:21:15 +0500

    Intermediate Questions JS

---
+++ Lecture/50 questions/script2.js
@@ -0,0 +1,23 @@
+// console.log(parseInt('10'))
+// console.log(Number('12343px'))
+
+
+// q 17
+
+
+console.log(0.1 + 0.2 !== 0.3);
+// due to floating point issue 
+
+
+// q 18
+
+// Handle precision 
+
+// let sum = (0.1 + 0.2).toFixed(2);
+
+// console.log(sum)
+
+
+
+// q 19
+
