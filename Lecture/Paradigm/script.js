50d6f827737ca58e2dc7b5102458aa56a64337fb
MuhammadHassanAnsari <muhammadhassanansari2008@gmail.com>
2026-09-05 00:52:50 +0500

    Paradigm Basic Concept

---
+++ Lecture/Paradigm/script.js
@@ -0,0 +1,153 @@
+// Programming paradigm : way of programming
+
+
+// let rr= [3,4,5,5,5];
+
+// for(let i = 0; i < rr.length; i++){
+//     console.log(rr[i])
+// }
+
+// console.log(rr)
+
+// in map we don't know how it works    
+// rr.map(ele =>{
+//     console.log(ele)
+// })
+
+
+
+
+// Produral Programming ---> procedure/ function mai warp kr do
+
+
+// function consolened(){
+//     console.log("Program is end!!...")
+// }
+
+
+// function checknumber(num){
+//     if(num === 0) {
+//         console.log(consolezero())
+//     }
+//     if(num > 0) {
+//         console.log(positive())
+//     } else{
+//         console.log(negative())
+//     }
+
+// }
+
+// function positive(){
+//     console.log("Number is Positive")
+//     consolened();
+
+// }
+
+// function consolezero() {
+//     console.log("Number is Zero")
+//     consolened();
+    
+// }
+
+// function negative(){
+//     console.log("Number is negative")
+//     consolened();
+
+// }
+
+// let num = 10;
+// checknumber(num);
+
+
+
+
+
+// Structure Programming ----> function use kr skte hai with proper sequence
+
+
+// function checknumber(num){
+//     if(num === 0) {
+//         console.log("Number is Zero")
+//     } else if (num > 0) {
+//         console.log("Number is Positive")
+        
+//     } else{
+//         console.log("Number is Negative")
+
+//     }
+
+
+//     console.log("Program Is End!!...")
+// }
+
+
+// checknumber(10);
+
+
+
+
+// functional Programming
+
+
+// function sum(a, b){
+//     return a + b;
+// }
+
+// let result = sum(1,5);
+// console.log(result)
+
+// functional programming m original array mai changes nhi krte 
+
+// let arr= [1,2,3,4,5];
+// function  doubleleemt(arr){
+//   let result = arr.map((elem) =>{
+//         return elem * 2;
+//     })
+
+
+//     return result;
+// }
+
+// let newarr = doubleleemt(arr);
+// console.log(newarr);
+// console.log(arr)
+
+
+
+
+// Object Oriented Programming overview 
+
+// let obj = {
+//     // ye singleton object hai 
+// }
+
+
+
+// class Car{
+//     constructor(name){
+//         this.name = name;
+//     }
+
+//     start(){
+//         console.log("Car is Start........")
+//     }
+//     stop(){
+//         console.log("Car is Stop........")
+//     }
+
+// }
+
+
+
+// let toyota = new Car('toyota')
+// console.log(toyota)
+
+
+
+// Event-Driven Programming 
+
+
+function consoleClick(){
+    console.log("I am Clicked");
+}
+
