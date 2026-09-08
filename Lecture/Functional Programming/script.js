39c1fefa44c1551c9470a2096fe1ab7d1a1f7354
MuhammadHassanAnsari <muhammadhassanansari2008@gmail.com>
2026-09-08 18:36:31 +0500

    Functional Programming

---
+++ Lecture/Functional Programming/script.js
@@ -0,0 +1,26 @@
+/// Pure Function
+
+// function sum(a, b){
+//     let sum =  a + b;
+//     console.log(sum)
+// }
+
+// sum(1,5)
+
+
+
+// // impure function 
+
+// let total = 0;
+
+
+// function increment() {
+//     total++;
+// }
+
+
+// increment();
+
+
+
+/// Imputability
