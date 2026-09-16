d476caff8b45679ed0763e0bcfb8a4d6427023dc
MuhammadHassanAnsari <muhammadhassanansari2008@gmail.com>
2026-09-16 22:21:21 +0500

    Practice Question

---
+++ Lecture/Functional Programming/practice.js
@@ -0,0 +1,63 @@
+// let numbers = [2, 4, 6, 8];
+// numbers.map(elem => {
+//     return elem * 2;
+
+// })
+
+
+// console.log(numbers);
+
+
+// let prices = [100, 200, 300, 400];
+
+// let result = prices.reduce((accumulator, currentValue) => {
+//     return accumulator + currentValue;
+// }, 0)
+
+// console.log(result);
+
+
+// let names = ["Ali", "Ahmed", "Sara", "Usman"];
+
+// let result1 = names.find(function (element) {
+//     return element === "Sara";
+// })
+
+
+// console.log(result1);
+
+// let numbers1 = [5, 12, 8, 20, 3, 15];
+// let res = numbers1.filter(element1 => {
+//     return element1 > 10;
+// })
+
+// console.log(res);
+
+
+// let numbers2 = [4, 9, 15, 22, 30];
+
+// let result = numbers2.find(elem =>{
+//     return elem > 10;
+// })
+
+// console.log(result);
+
+
+
+
+// let names = ["Ali", "Ahmed", "Sara", "Usman"];
+
+// let result = names.includes("Sara");
+
+// console.log(result)
+
+
+// let numbers = [2, 5, 8, 11, 14, 17];
+
+// let result = numbers.filter(elem =>{
+//     return  elem % 2 === 0
+// })
+
+// console.log(result)
+
+
