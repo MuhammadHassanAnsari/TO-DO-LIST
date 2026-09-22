c7e187b5660328f581dcf22ef33fdd414ea7f3fe
MuhammadHassanAnsari <muhammadhassanansari2008@gmail.com>
2026-09-22 21:19:07 +0500

    Inheritance

--- Lecture/OOPS/class.js
+++ Lecture/OOPS/class.js
@@ -1,96 +1,131 @@
-// class Car{
-//     constructor(name,color,mileage){
-//         this.name = name;
-//         this.color = color;
-//         this.mileage = mileage;
-//     }
+// // class Car{
+// //     constructor(name,color,mileage){
+// //         this.name = name;
+// //         this.color = color;
+// //         this.mileage = mileage;
+// //     }
 
-//     start(){
-//         console.log(`${this.name} is starting....`)
-//     }
+// //     start(){
+// //         console.log(`${this.name} is starting....`)
+// //     }
 
-//     stop(){
-//         console.log(`${this.name} is stopping....`)
-//     }
-// }
+// //     stop(){
+// //         console.log(`${this.name} is stopping....`)
+// //     }
+// // }
 
 
-// let bmw = new Car("BMW","Darkblue",54);
-// let toyota = new Car("Toyota","Silver",24);
-// let bugati = new Car("Bugati","Brown",5);
+// // let bmw = new Car("BMW","Darkblue",54);
+// // let toyota = new Car("Toyota","Silver",24);
+// // let bugati = new Car("Bugati","Brown",5);
  
 
 
-// console.log(bmw);
-// console.log(toyota);
-// console.log(bugati);
+// // console.log(bmw);
+// // console.log(toyota);
+// // console.log(bugati);
 
 
 
 
 
-// FOUR PILAR OF OOPS 
+// // FOUR PILAR OF OOPS 
 
-// 1- Abstraction 
+// // 1- Abstraction 
 
-// class Car {
-//     #fuel = 100;
+// // class Car {
+// //     #fuel = 100;
 
-//     #burnfuel(){
-//         this.#fuel -= 1;
+// //     #burnfuel(){
+// //         this.#fuel -= 1;
+// //     }
+
+
+// //     start() {
+// //         this.#burnfuel();
+// //         console.log("Car is Starting...")
+// //     }
+
+// // }
+
+
+// // let bugati = new Car();
+// // bugati.start();
+// // console.log(bugati);
+
+
+
+
+// class BankAccount{
+
+//     #balance = 100;
+
+//     constructor(holdersname, balance = 100) {
+//         this.holdersname = holdersname;
+//         this.#balance = balance;
 //     }
 
 
-//     start() {
-//         this.#burnfuel();
-//         console.log("Car is Starting...")
+//     deposit(){
+//         this.#balance += balance;
+//     }
+
+
+//     set Balance(balance){
+
+//         if(isNaN(balance)){
+//             console.log("Invalid Balance");
+//             return;
+//         }
+//         console.log("Balance Set Sucessfully");
+//         this.#balance = balance;
+//     }
+
+
+//     get balance(){
+//         return this.#balance;
 //     }
 
 // }
 
+// let hassanaccount = new BankAccount("Hassan", 500);
 
-// let bugati = new Car();
-// bugati.start();
-// console.log(bugati);
+// // hassanaccount.setBalance(1000);
+// // hassanaccount.#balance = 3232; not access because we make it private
+// // hassanaccount.deposit(400)
+// // console.log(hassanaccount.getbalance());
 
 
+// 3. Inheritance 
 
 
-class BankAccount{
-
-    #balance = 100;
-
-    constructor(holdersname, balance = 100) {
-        this.holdersname = holdersname;
-        this.#balance = balance;
+class Car{
+    constructor(brand,color,battery){
+        this.brand = brand;
+        this.color = color;
     }
 
 
-    deposit(){
-        this.#balance += balance;
+    start(){
+        console.log(`${this.brand} is starting....`)
     }
-
-
-    set Balance(balance){
-
-        if(isNaN(balance)){
-            console.log("Invalid Balance");
-            return;
-        }
-        console.log("Balance Set Sucessfully");
-        this.#balance = balance;
+    
+    stop(){
+        console.log(`${this.brand} is stopping....`)
     }
-
-
-    get balance(){
-        return this.#balance;
-    }
-
 }
 
-let hassanaccount = new BankAccount("Hassan", 500);
 
-// hassanaccount.setBalance(1000);
-// hassanaccount.#balance = 3232; not access because we make it private
-// hassanaccount.deposit(400)
-// console.log(hassanaccount.getbalance());
+class ELectricCar extends Car   {
+    constructor(brand,color,battery){
+        super(brand,color)  // super use kia taake parent class ke constructor ko call kr sake 
+        this.battery = battery;
+    }
+     charging(){
+        console.log(`${this.brand} is charging....`)
+    }
+}
+
+
+let tesla = new ELectricCar("Tesla", 'Black',99);
+console.log(tesla);
