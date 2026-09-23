f590670db0f3f2079bd59d4b4102564d25508fc3
MuhammadHassanAnsari <muhammadhassanansari2008@gmail.com>
2026-09-23 23:06:30 +0500

    JSON

---
+++ Lecture/API/script.js
@@ -0,0 +1,18 @@
+// let obj = {
+//     name : 'Hassan Ansari',
+//     age : 21,
+// }
+
+
+// console.log(obj)
+
+// JSON always use double quote
+// number, string, null, boolyean, object, array
+
+
+let json = `{
+   "name" : "Hassan Ansari",
+    "age" : "21"
+}`;
+
+console.log(json)
